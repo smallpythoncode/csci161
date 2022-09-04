b2e5aa45aac393f2383c029d6e9ed9e073975951
smallpythoncode <smallpythoncode@gmail.com>
2022-09-03 20:12:08 -0500

    Added class package boilerplate

---
+++ class_notes/20220824_first_class.py
@@ -0,0 +1,8 @@
+"""
+get virtual machine that runs linux mint
+python 3 interpreter
+any editor
+- g edit recommended
+
+virtual box
+"""
---
+++ tinkering/tinkering.py
@@ -0,0 +1,8 @@
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+# All work and no play makes Jack a dull boy.
