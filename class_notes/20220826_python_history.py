a3fe4c9a873500a78178cd339a1b396420346aa0
smallpythoncode <smallpythoncode@gmail.com>
2022-09-04 14:36:38 -0500

    Notes for 20220826.

---
+++ class_notes/20220826_python_history.py
@@ -0,0 +1,60 @@
+"""
+Python supports the same escape sequences as other languages
+\n example
+
+input function is built into language
+- will come out as string
+- may need further processing for, say, ints
+- allows for prompts
+  - tells the user what sort of info is expected
+
+Errors
+1. Syntax error (typos, basically)
+- will give you an error message to tell you what is wrong and where
+- develop your code incrementally, break problem into sub-problems
+2. Runtime errors
+- syntactically is fine, but a wrong type is fed to a function
+- tabs are different to multiple spaces
+- NameError: program tries to use a variable that has not been defined
+3. Logic errors (bugs)
+- computers do exactly what you tell them to do
+- no runtime or syntax errors, but the output is not what is expected
+
+IDE
+- Nordlie doesn't use one
+- he used a text editor and runs it in the command line
+- good to be able to use bare bones
+
+- Analog computers use ranges of electrical current
+  - creates noise in copying, same with cassettes
+- digital computers use binary switches
+  - still creates noise, but the noise can be easily detected and removed
+  - limits to what we can represent with these computers
+    - only 2 digits, not a range of el. currents like analog
+
+machine instructions are just binary strings
+assembler vs compiler
+assembler - 1 for 1 as far as the language instructions
+- variables need to manually moved/called
+compiler - one instruction is likely a compilation of other "smaller"
+  instructions
+- variables are stored and can be easily recalled
+
+clocks tick at a specific frequency
+
+Scripting languages are used to execute programs w/o need for compilation
+- via an interpreter
+Python was developed with simplicity and readability in mind
+- still focused on being powerful
+- easy to learn and logical
+- not a toy language, no constraints intended on power or sophistication
+- many, many, many support packages available
+- open-source
+- v1.0 released in 1994
+- v2.0 released in 2000
+  - some integrated memory allocation and garbage clean-up
+  - deprecated
+- v3.0 is not backwards compatible
+
+Tiobe index gauges how popular a language is (www.tiobe.com)
+"""
