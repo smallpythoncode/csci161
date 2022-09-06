b1e6c9c685fbc2cd4a66568b586622146482ea7d
smallpythoncode <smallpythoncode@gmail.com>
2022-09-06 18:57:05 -0500

    1. lecture notes 20220902; 2. lab01 complete win

---
+++ class_notes/20220902.py
@@ -0,0 +1,32 @@
+"""
+indentation is important...obviously...you idiot.
+
+ternary operation - middle is executed first
+    expr_when_true if condition else expr_when_false
+
+loops - executes a block of code through a number of iterations
+while loops - based on a boolean
+- sentinel values check a condition that dictates whether the loop will exec.
+
+Turing test - indistinguishable from a human
+
+for loops are ideal for iterating
+- ideal for containers
+- for dicts, keys are iterated
+- reversed() iterates in reverse order...no shit
+
+range()
+- includes first, goes up to but doesn't include last
+
+while vs. for
+- for: counting; iterating
+- while: known iterations; expecting input
+
+Break statements can be used to break a loop if a condition is met
+continue statement causes an immediate jump to the loop header statement
+- skips the rest of the code block, starts the next iteration
+
+Loop else statements
+
+enumerate() - gets both the value and the index
+"""
---
+++ labs/lab01/jahnke_kenneth_lab01.py
@@ -0,0 +1,27 @@
+"""Jahnke, Student ID: 0808831
+kenny.jahnke@ndus.edu / smallpythoncode@gmail.com
+CSCI 161, Fall, Lecture Sect 02, Lab Sect L03 (Online)
+Lab 01
+Copyright (C) 2022 Kenneth Jahnke
+
+Assignment:
+    Write a "Hello, World!" program.
+"""
+
+
+def hello_world():
+    """For good luck!
+
+    :rtype: None
+    """
+    print("Hello, World!")
+
+
+def main():
+    hello_world()
+
+
+if __name__ == "__main__":
+    main()
+
+# All work and no play makes Jack a dull boy.
