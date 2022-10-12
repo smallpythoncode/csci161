76a3196e6c0f9eda4fa395a16a50ab3b2046b08b
smallpythoncode <smallpythoncode@gmail.com>
2022-10-11 22:19:19 -0500

    ass. 6 complete.

--- assignments/assignment6/jahnke_kenneth_6.py
+++ assignments/assignment6/jahnke_kenneth_6.py
@@ -1,59 +1,177 @@
 """Jahnke, Student ID: 0808831
 kenny.jahnke@ndus.edu / smallpythoncode@gmail.com
 CSCI 161, Fall, Lecture Sect 02, Lab Sect L03 (Online)
-# FIXME Assignment X
+Assignment 6
 Copyright (C) 2022 Kenneth Wayne Jahnke
 
+Functions:
+    display()
+        - User creates a dictionary, then it is printed.
+        - Option 1
+    search()
+        - Checks if a user-input string is a value in the_dict.
+        - Option 2
+    sort()
+        - Sorts the_dict by key.
+        - Option 3
+    lists()
+        - Prints keys and values of the_dict in separate sorted lists.
+        - Option 4
+    break_loop()
+        - SEE NOTE IN FUNCTION DEFINITION.
+        - Option 5
+    menu()
+        - Prints a menu and prompts the user for an option.
+    main()
 """
 
 
-# TODO
-def display():  # option 1
-    """ # TODO
+def display(user_dict=None):  # option 1
+    """User creates a dictionary, then it is printed.
 
-    :return:
+    When prompted for a key, user must enter an int or the string
+    'exit', which stops prompting user for more key-value pairs.
+    For each key, user enters a string.
+
+    :param user_dict:
+        A user dict to add key-value pairs to. If argument is not
+        passed (None), a new dict will be created.
+    :type user_dict: dict or None
+    :except ValueError:
+        User attempted to enter a character that could not be
+        converted to an int. Addressed by looping back to prompt.
+    :return: The dict created by the user.
+    :rtype: dict
     """
-    """
-    the_dict = {}
+    if user_dict is None:
+        user_dict = {}
     print("Let's create a dictionary.\n"
-          "You will be asked for integers to be used as keys and strings to\n"
-          "  be used as keys.\n"
+          "You will be asked for integers to be used as keys and\n"
+          "    strings to be used as values.\n"
           "Note: previously entered dictionaries will be overwritten.\n"
           "To quit, type 'exit' at the key prompt.")
 
     while True:
-        key = input("Enter a key: ").lower()
-        if len(key) == 0:  # if user enters nothing
-            print("Please enter an integer or 'exit'.")
-            continue
-        # try:  # FIXME
+        key = input("Enter a key: ").lower().strip()
+        if key == "exit":
+            break
+        try:
+            key = int(key)
+        except ValueError:
+            print("Invalid key entry. Enter an integer or 'exit'.")
+        else:
+            keep_old_key = False  # in case of duplicate keys
+            if key in user_dict.keys():
+                print("That is already a key in your dictionary.")
+                while True:
+                    key_overwrite = input(
+                        "Do you want to overwrite the old key? (y/n): "
+                        ).lower().strip()
+                    if key_overwrite not in ["y", "yes", "n", "no"]:
+                        print("    Invalid response.")
+                    elif key_overwrite in ["n", "no"]:
+                        keep_old_key = True
+                        break
+                    else:
+                        break
+            if keep_old_key:  # in case of duplicate keys
+                continue
+
+            while True:
+                value = input("Enter a value: ")
+                value_affirmed = True
+                if len(value) == 0:
+                    print("You entered an empty string for a value.")
+                    while True:
+                        verify_empty = input("Do you want to keep it? "
+                                             "(y/n): ").lower().strip()
+                        if verify_empty not in ["y", "yes", "n", "no"]:
+                            print("    Invalid response.")
+                        elif verify_empty in ["n", "no"]:
+                            value_affirmed = False
+                            break
+                        else:
+                            break
+                if value_affirmed:
+                    user_dict[key] = value
+                    break
+
+    print("Your dictionary:", user_dict, sep="\n")
+    return user_dict
+
+
+def search(the_dict):  # option 2
+    """Checks if a user-input string is a value in the_dict.
+
+    :param dict the_dict:
+        The dict whose values are to be searched for a match of the
+        user-input value.
+    :return: None
     """
-    print("called")  # FIXME - return original code, for testing only
+    print("Enter a string to see if it is a value in the dictionary.")
+    user_str = input("String: ")
+
+    for key, value in the_dict.items():
+        if user_str == value:
+            print(f"'{value}' is the value of key {key}.")
+            break
+    else:
+        print(f"{user_str} is not a value in the dictionary.")
 
 
-# TODO
-def search():  # option 2
-    """ # TODO
+def sort(the_dict):  # option 3
+    """Sorts the_dict by key.
 
+    .. note::
+        Per TA guidance, the original list is sorted, i.e., this func
+        does not sort a copy while keeping the original intact.
+
+    :param dict the_dict:
+        The dict to be sorted.
     :return:
     """
-    pass
+    print(f"Original Dictionary:\n{the_dict}")
+
+    # sorting criteria
+    temp_dict = the_dict.copy()
+    sort_keys = sorted(temp_dict.keys())
+
+    # remove elements of old list
+    for i in list(the_dict):
+        del the_dict[i]
+
+    # add sorted elements
+    for i in sort_keys:
+        for key, value in temp_dict.items():
+            if key == i:
+                the_dict[key] = value
+
+    print(f"Key-Sorted Dictionary:\n{the_dict}")
 
 
-# TODO
-def sort():  # option 3
-    """ # TODO
+def lists(the_dict):  # option 4
+    """Prints keys and values of the_dict in separate sorted lists.
 
-    :return:
+    :param dict the_dict:
+        The dict whose keys and values are to be sorted into lists.
+    :return: None
     """
-    # pass
-    print("opt 3 called")  # FIXME - for testing only
+    the_keys = sorted(the_dict.keys())
+    the_values = sorted(the_dict.values())
+    print("Sorted keys:", the_keys)
+    print("Sorted values:", the_values)
 
-# TODO
-def lists():  # option 4
-    """ # TODO
 
-    :return:
+def break_loop():  # option 5
+    """SEE NOTE IN FUNCTION DEFINITION.
+
+    ..note::
+        For Assignment 6, this func is a "trigger" placeholder for
+        Option 5 in options dict in main(), i.e., the break_loop object
+        is referenced without call as only an indication to break the
+        main loop.
+
+    :return: None
     """
     pass
 
@@ -71,47 +189,19 @@ def menu(option_set):
     print("\nMenu:")
     for key, value in option_set.items():
         print("    Option ", key, ": ", value["prompt"], sep="")
-    prompt = input("Enter an option number: ")
+    option = input("Enter an option number: ")
 
     while True:
-        if prompt in option_set:
+        if option in option_set:
             print()
-            return prompt
+            return option
         else:
-            print(f"    {prompt} is not a valid option.")
-            prompt = input("Enter an option number: ")
-
-
-class LoopBreak(Exception):
-    """The exception raised by the break_loop func.
-
-    The triggers within the options dict in main indicate what action to
-    take based on the option chosen by the user. To exit the program,
-    the option prompt loop must be broken. Since the 'break' keyword
-    cannot be stored as a dictionary value, another method is required
-    to 'call' a break. The trigger to break the loop calls break_loop
-    which raises this exception which is then handled by breaking the
-    loop.
-    """
-    pass
-
-
-def break_loop():  # option 5
-    """Raises the LoopBreak exception.
-
-    The 'break' keyword cannot be stored as dict value. This func is
-    called to raise the LoopBreak exception created to be handled by
-    inserting a break.
-
-    :raise LoopBreak:
-        The exception is designed to be handled by breaking a loop.
-    :return: None
-    """
-    raise LoopBreak
+            print(f"    {option} is not a valid option.")
+            option = input("Enter an option number: ")
 
 
 def main():
-    options = {  # FIXME - modify options, keep break_loop
+    options = {
         "1": {
             "prompt": "Create and Display the Dictionary",
             "trigger": display
@@ -131,22 +221,21 @@ def main():
         },
         "5": {"prompt": "Exit", "trigger": break_loop}
     }
-    dict_init = False  # display() has been called, a dict initialized
 
+    user_dict = None
     while True:
-        print(dict_init)  # FIXME - delete, for testing only
         selected_option = menu(options)
         trigger = options[selected_option]["trigger"]
-        if trigger is display:
-            dict_init = True
-        try:
-            trigger()
-        except LoopBreak:
+        if trigger is break_loop:  # see def break_loop() for more info
             print("Goodbye.")
             break
+        elif trigger is display:
+            user_dict = trigger(user_dict)  # display()
+        elif not user_dict:
+            print("No dictionary initialized.\n"
+                  "Select Option 1 to initialize.")
         else:
-            if not dict_init:
-                print("no dict")
+            trigger(user_dict)
 
 
 if __name__ == "__main__":
